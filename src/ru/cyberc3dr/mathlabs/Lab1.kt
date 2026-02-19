55b4b1871ffe6ed6dc44725eba76a8831985c8f6
cyberc3dr <geo3gamer@gmail.com>
2026-02-19 20:11:06 +0300

    fix: условие сходимости

--- src/ru/cyberc3dr/mathlabs/Lab1.kt
+++ src/ru/cyberc3dr/mathlabs/Lab1.kt
@@ -4,15 +4,16 @@ import kotlin.math.abs
 import kotlin.math.acos
 import kotlin.math.cos
 import kotlin.math.pow
+import kotlin.math.sin
 import kotlin.math.sqrt
 
 object Lab1 {
 
-    private val epsilon = 0.001 // Точность вычислений (критерий остановки)
+    private val epsilon = 0.001   // Точность вычислений (критерий остановки)
 
-    private val h = 0.1          // Шаг для локализации корня
-    private val x0 = 0.0         // Начало отрезка поиска
-    private val x1 = 1.0         // Конец отрезка поиска (используется в локализации)
+    private val h = 0.01          // Шаг для локализации корня
+    private val x0 = 0.0          // Начало отрезка поиска
+    private val x1 = 1.0          // Конец отрезка поиска (используется в локализации)
 
     // Определение функции f(x)
     fun f(x: Double) = acos(x) - sqrt(1 - 0.3 * x.pow(3))
@@ -23,9 +24,13 @@ object Lab1 {
     // Эквивалентная функция x = fi(x) для метода простых итераций
     fun fi(x: Double) = cos(sqrt(1 - 0.3 * x.pow(3)))
 
+    // Производная fi'(x) для проверки условия сходимости метода простых итераций
+    fun fip(x: Double) = -sin(sqrt(1 - 0.3 * x.pow(3))) * (0.45 * x.pow(2)) / sqrt(1 - 0.3 * x.pow(3))
+
     @JvmStatic
     fun main(args: Array<String>) {
         println("Выполним шаговый метод локализации корня.")
+        println("Интервал поиска: [$x0, $x1], шаг: $h")
 
         var a = x0
         var b = a + h
@@ -36,16 +41,17 @@ object Lab1 {
             b += h
         }
 
-        println("Получаем интервал: [$a, $b]")
+        println("Получаем интервал: [${"%.2f".format(a)}, ${"%.2f".format(b)}]")
 
-        method1(a, b)
-        method2(a, b)
-        method3(a, b)
-        method4(a, b)
+        bisectionMethod(a, b)
+        newtonMethod(b)
+        simpleIterationMethod(a, b)
+        secantMethodRight(a, b)
+        secantMethodLeft(a, b)
     }
 
-    // Метод половинного деления (бисекции)
-    fun method1(start: Double, end: Double) {
+    // Метод половинного деления
+    fun bisectionMethod(start: Double, end: Double) {
         println("\nМетод 1: половинное деление")
 
         var a = start
@@ -74,10 +80,10 @@ object Lab1 {
     }
 
     // Метод Ньютона
-    fun method2(a: Double, b: Double) {
+    fun newtonMethod(end: Double) {
         println("\nМетод 2: Ньютона")
 
-        var x = b
+        var x = end
         var xnext = x - f(x) / fp(x)
         var iterations = 0
 
@@ -95,10 +101,20 @@ object Lab1 {
     }
 
     // Метод простых итераций
-    fun method3(a: Double, b: Double) {
-        println("\nМетод 3: простых итераций")
+    fun simpleIterationMethod(start: Double, end: Double) {
+        println("\nМетод 3: Простых итераций")
 
-        var x = a
+        println("Проверим условие сходимости")
+
+        // Проверка условия сходимости
+        if(abs(fip(start)) < 1 && abs(fip(end)) < 1) {
+            println("Условие сходимости выполняется, можно применять метод простых итераций.")
+        } else {
+            println("Условие сходимости не выполняется.")
+            return
+        }
+
+        var x = start
         var xnext = fi(x)
         var iterations = 0
 
@@ -115,12 +131,12 @@ object Lab1 {
         println("Кол-во приближений: ${iterations + 1}")
     }
 
-    // Метод секущих (хорд)
-    fun method4(a: Double, b: Double) {
-        println("\nМетод 4: секущих")
+    // Метод секущих (фиксированный правый конец)
+    fun secantMethodRight(start: Double, end: Double) {
+        println("\nМетод 4: Хорд (секущих) - фиксированный правый конец")
 
-        var x = a
-        var xnext = x - f(x) / (f(x) - f(b)) * (x - b)
+        var x = start
+        var xnext = x - f(x) / (f(x) - f(end)) * (x - end)
         var iterations = 0
 
         while(abs(xnext - x) > epsilon) {
@@ -128,7 +144,28 @@ object Lab1 {
             println("Итерация $iterations: x=${"%.6f".format(x)}, f(x)=${"%.6f".format(f(x))}")
 
             x = xnext
-            xnext = x - f(x) / (f(x) - f(b)) * (x - b)
+            xnext = x - f(x) / (f(x) - f(end)) * (x - end)
+        }
+
+        println("Итерация ${iterations + 1}: x=${"%.6f".format(xnext)}, f(x)=${"%.6f".format(f(xnext))}")
+        println("Приближенное значение корня: ${"%.3f".format(xnext)}")
+        println("Кол-во приближений: ${iterations + 1}")
+    }
+
+    // Метод секущих (фиксированный левый конец)
+    fun secantMethodLeft(start: Double, end: Double) {
+        println("\nМетод 4: Хорд (секущих) - фиксированный левый конец")
+
+        var x = start
+        var xnext = x - f(x) / (f(x) - f(end)) * (x - end)
+        var iterations = 0
+
+        while(abs(xnext - x) > epsilon) {
+            iterations++
+            println("Итерация $iterations: x=${"%.6f".format(x)}, f(x)=${"%.6f".format(f(x))}")
+
+            x = xnext
+            xnext = x - f(x) / (f(x) - f(end)) * (x - end)
         }
 
         println("Итерация ${iterations + 1}: x=${"%.6f".format(xnext)}, f(x)=${"%.6f".format(f(xnext))}")
